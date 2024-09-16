c9a471f78df15fe5321afc94770587cde000de1d
Kimball Oyler <167648368+koyler88@users.noreply.github.com>
2024-09-16 09:30:04 -0600

    Create rough styling of calculator divs (UI)

--- javascript.js
+++ javascript.js
@@ -14,9 +14,11 @@ function divide(x,y) {
     return x / y
 }
 
+/*
 let numberOne = a
 let numberTwo = b
 let operator = "+"
+*/
 
 function operate(operator, x, y) {
     if (operator === "+") {
@@ -31,4 +33,25 @@ function operate(operator, x, y) {
     else if (operator === "/") {
         return divide(x,y)
     }
+}
+
+const buttonContainer = document.querySelector('.buttonContainer')
+
+for (let i = 1; i < 20; i++) {
+    newDiv = document.createElement('button')
+    if (i === 4 || i === 8 || i === 12 || i === 16
+        || i === 19 ) {
+            newDiv.classList.add('operator')
+        }
+    else if (i === 1 || i === 2 || i === 3) {
+        newDiv.classList.add('blue')
+    }
+    else if (i === 17) {
+        newDiv.classList.add('zero')
+        newDiv.textContent = '0'
+    }
+    else {
+        newDiv.classList.add('cell')
+    }
+    buttonContainer.appendChild(newDiv)
 }
