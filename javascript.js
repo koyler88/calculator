cb982c9b3a1fd8e02184332eb11271a529fee7f9
Kimball Oyler <167648368+koyler88@users.noreply.github.com>
2024-09-16 15:54:41 -0600

    Allow multiple operations before "="

--- javascript.js
+++ javascript.js
@@ -1,59 +1,3 @@
-function add(x,y) {
-    return parseFloat(x) + parseFloat(y)
-}
-
-function subtract(x,y) {
-    return parseFloat(x) - parseFloat(y)
-}
-
-function multiply(x,y) {
-    return parseFloat(x) * parseFloat(y)
-}
-
-function divide(x,y) {
-    return parseFloat(x) / parseFloat(y)
-}
-
-
-
-function operate(operator, x, y) {
-    if (operator === "+") {
-        return add(x,y)
-    }
-    else if (operator === "-") {
-        return subtract(x,y)
-    }
-    else if (operator === "*") {
-        return multiply(x,y)
-    }
-    else if (operator === "/") {
-        return divide(x,y)
-    }
-}
-
-const display = document.querySelector(".display")
-
-displayValue = ''
-
-const clear = document.querySelector(".clear")
-
-clear.addEventListener('click', () => {
-    display.textContent = 0;
-    displayValue = ''
-})
-
-const plusMinus = document.querySelector(".plusMinus")
-plusMinus.addEventListener('click', () => {
-    display.textContent = display.textContent * -1
-    displayValue = displayValue * -1
-})
-
-const percentage = document.querySelector(".percentage")
-percentage.addEventListener('click', () => {
-    display.textContent = display.textContent / 100
-    displayValue = displayValue / 100
-})
-
 const numberZero = document.querySelector(".zero")
 numberZero.value = 0
 const numberOne = document.querySelector(".one")
@@ -75,18 +19,71 @@ numberEight.value = 8
 const numberNine = document.querySelector(".nine")
 numberNine.value = 9
 
-/*
+function add(x,y) {
+    return parseFloat(x) + parseFloat(y)
+}
+
+function subtract(x,y) {
+    return parseFloat(x) - parseFloat(y)
+}
+
+function multiply(x,y) {
+    return parseFloat(x) * parseFloat(y)
+}
+
+function divide(x,y) {
+    return parseFloat(x) / parseFloat(y)
+}
+
+
+
+function operate(operator, x, y) {
+    if (operator === "add") {
+        return add(x,y)
+    }
+    else if (operator === "subtract") {
+        return subtract(x,y)
+    }
+    else if (operator === "multiply") {
+        return multiply(x,y)
+    }
+    else if (operator === "divide") {
+        return divide(x,y)
+    }
+}
+
+const display = document.querySelector(".display")
+
+const clear = document.querySelector(".clear")
+
+clear.addEventListener('click', () => {
+    display.textContent = 0;
+    displayValue = ''
+    firstNumber = null
+    secondNumber = null
+})
+
+const plusMinus = document.querySelector(".plusMinus")
+plusMinus.addEventListener('click', () => {
+    display.textContent = display.textContent * -1
+    displayValue = displayValue * -1
+})
+
+const percentage = document.querySelector(".percentage")
+percentage.addEventListener('click', () => {
+    display.textContent = display.textContent / 100
+    displayValue = displayValue / 100
+})
+
+
 const decimal = document.querySelector(".decimal")
 decimal.value = "."
 
 decimal.addEventListener('click', (e) => {
     element = e.target
-    console.log(element.value)
     displayValue += element.value
-    console.log(displayValue)
     display.textContent = displayValue;
 })
-*/
 
 
 const numbers = document.querySelectorAll(".number")
@@ -97,9 +94,10 @@ numbers.forEach(button =>  button.addEventListener('click', (e) => {
     display.textContent = parseFloat(displayValue);
 }))
 
-firstNumber = 0
+firstNumber = null
 operation = ''
-secondNumber = 0
+secondNumber = null
+displayValue = ''
 
 const division = document.querySelector(".divide")
 const multiplication = document.querySelector(".multiply")
@@ -108,27 +106,63 @@ const addition = document.querySelector(".plus")
 const equal = document.querySelector(".equals")
 
 addition.addEventListener('click', () => {
-    firstNumber = displayValue;
-    displayValue = ''
-    operation = "add"
+    if (firstNumber === null) {
+        firstNumber = displayValue;
+        displayValue = ''
+        operation = "add"
+    }
+    else {
+        secondNumber = displayValue;
+        display.textContent = operate(operation, firstNumber,secondNumber)
+        firstNumber = operate(operation, firstNumber,secondNumber)
+        displayValue = ''
+        operation = 'add'
+    }
 })
 
 subtraction.addEventListener('click', () => {
-    firstNumber = displayValue;
-    displayValue = ''
-    operation = 'subtract'
+    if (firstNumber === null) {
+        firstNumber = displayValue;
+        displayValue = ''
+        operation = "subtract"
+    }
+    else {
+        secondNumber = displayValue;
+        display.textContent = operate(operation, firstNumber,secondNumber)
+        firstNumber = operate(operation, firstNumber,secondNumber)
+        displayValue = ''
+        operation = 'subtract'
+    }
 })
 
 multiplication.addEventListener('click', () => {
-    firstNumber = displayValue;
-    displayValue = ''
-    operation = 'multiply'
+    if (firstNumber === null) {
+        firstNumber = displayValue;
+        displayValue = ''
+        operation = "multiply"
+    }
+    else {
+        secondNumber = displayValue;
+        display.textContent = operate(operation, firstNumber,secondNumber)
+        firstNumber = operate(operation, firstNumber,secondNumber)
+        displayValue = ''
+        operation = 'multiply'
+    }
 })
 
 division.addEventListener('click', () => {
-    firstNumber = displayValue;
-    displayValue = ''
-    operation = 'divide'
+    if (firstNumber === null) {
+        firstNumber = displayValue;
+        displayValue = ''
+        operation = "divide"
+    }
+    else {
+        secondNumber = displayValue;
+        display.textContent = operate(operation, firstNumber,secondNumber)
+        firstNumber = operate(operation, firstNumber,secondNumber)
+        displayValue = ''
+        operation = 'divide'
+    }
 })
 
 equal.addEventListener('click', () => {
